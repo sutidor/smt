c6da1903383ac30029853bcfeda4b2c67c5dcc6f
The Louie <github@louie.se>
2020-06-08 12:38:29 +0200

    add new strategy

--- routes/strategies.js
+++ routes/strategies.js
@@ -5,7 +5,7 @@
  *
  * @author:     Ilya Chubarov [@agoalofalife] <agoalofalife@gmail.com>
  * @license:    This code and contributions have 'GNU General Public License v3'
- * 
+ *
  */
 module.exports = {
 	"direct_new_followers": require("../modules/mode/direct_new_followers"),
@@ -14,5 +14,6 @@ module.exports = {
 	"likemode_superlike": require("../modules/mode/likemode_superlike"),
 	"fdfmode_classic": require("../modules/mode/fdfmode_classic"),
 	"comment_mode": require("../modules/mode/commentmode_classic"),
-	"likemode_competitor_users": require("../modules/mode/likemode_competitor_users")
+	"likemode_competitor_users": require("../modules/mode/likemode_competitor_users"),
+	"followmode_competitor_followers": require("../modules/mode/followmode_competitor_followers")
 };
